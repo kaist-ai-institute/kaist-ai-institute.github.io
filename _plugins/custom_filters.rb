36d855cc7572f352de02fdcfdf38af428d5d85b4
Yoojin Hong <dbwk18@kaist.ac.kr>
2023-07-31 18:40:02 +0900

    Preserve line break & add manual

--- _plugins/custom_filters.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module PreserveLineBreaksFilter
-    def preserve_line_breaks(input)
-      input.gsub("\n", "<br>")
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::PreserveLineBreaksFilter)
